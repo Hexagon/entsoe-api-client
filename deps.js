a6851fdeee2deb0f7b98b8cf3c86c51be6ce8d21
Hexagon <hexagon@56k.guru>
2023-01-30 22:45:29 +0100

    Disable zip.js workers

--- deps.js
+++ deps.js
@@ -1,2 +1,2 @@
 export { parse } from "https://deno.land/x/xml@2.1.0/mod.ts";
-export { ZipReader, Uint8ArrayReader, TextWriter } from "https://deno.land/x/zipjs@v2.6.61/index.js"
+export { ZipReader, Uint8ArrayReader, TextWriter } from "https://deno.land/x/zipjs@v2.6.61/lib/zip-no-worker.js";
